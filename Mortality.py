5ad4cec8633c82d2f841e666ed743664b9b4055c
Zachary Lasley <116044492+Zlasley@users.noreply.github.com>
2024-07-27 16:01:57 -0500

    Update Mortality.py

--- Mortality.py
+++ Mortality.py
@@ -30,7 +30,7 @@ df_null = df.isnull().sum().sum()
 
 print(f"how many null values are in the df: {df_null}")
 
-
+print(f"How many rows: {df.len()}")
 
 # Set-up the one-hot encoder method
 categorical_features = ['---']
